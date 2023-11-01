74d077bbd3846c783503a7b11d677847c264873d
Button501 <51501719+SodeButton@users.noreply.github.com>
2023-11-01 16:51:04 +0900

    不要なコメント削除

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,11 +1,5 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
     apiKey: "AIzaSyChpOsuTg7f_EsyJqjShzcY8POusMYiPK4",
     authDomain: "phaser-multiplayer-test.firebaseapp.com",
@@ -17,7 +11,6 @@ const firebaseConfig = {
     measurementId: "G-LRGW16MJE9"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
 export {app};
