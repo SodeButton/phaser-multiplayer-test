e83362d611aa1d24867fa639edf60787551ef3fb
Button501 <51501719+SodeButton@users.noreply.github.com>
2024-03-01 14:30:47 +0900

    更新

--- src/scenes/TestScene.ts
+++ src/scenes/TestScene.ts
@@ -8,6 +8,8 @@ import imgShadow from "../assets/shadow.png";
 import imgCoin from "../assets/coin.png";
 import imgCoinShadow from "../assets/coin-shadow.png";
 
+const playerColors = ["red", "blue", "green", "yellow", "orange", "purple"];
+
 export default class TestScene extends Phaser.Scene {
   constructor() {
     super("TestScene");
@@ -33,7 +35,8 @@ export default class TestScene extends Phaser.Scene {
 
     new Coin(this, 400, 300);
 
-    this.createPlayer();
+    this.logIn(); // ログイン処理
+
     this.initInput();
     this.initGame();
   }
@@ -42,11 +45,36 @@ export default class TestScene extends Phaser.Scene {
     super.update(time, delta);
   }
 
+  logIn() {
+    if (this.firebase.getUser()) return;
+
+    this.firebase.signInAnonymously().then();
+
+    this.firebase.onLoggedIn(() => {
+      console.log("Logged in");
+      this.createPlayer();
+    });
+  }
+
   initInput() {}
 
   initGame() {}
 
-  createPlayer() {}
+  createPlayer() {
+    const playerId = this.firebase.getUser()?.uid;
+    if (playerId == undefined) return;
+    this.firebase
+      .setUserData(playerId, {
+        id: playerId,
+        name: this.createName(),
+        direction: "right",
+        color: this.randomFromArray(playerColors),
+        x: 400,
+        y: 300,
+        coins: 0,
+      })
+      .then();
+  }
 
   createName() {
     const prefix = this.randomFromArray([
